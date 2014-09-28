1f3567aa9681066cb4e60d25846dc39410384d7c
Kshitij Limaye <klimaye@gmail.com>
2014-09-28 00:07:42 -0500

    initial commit for async await with gtk#2.0

---
+++ PlayWithAsyncAwait/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PlayWithAsyncAwait
+{
+	class MainClass
+	{
+		static async Task<int> SomethingThatTakesTime ()
+		{
+			await Task.Delay(5000);
+			int counter = 20;
+			while(counter >= 0)
+			{
+				Console.WriteLine("doing something else {0}",counter--);
+				await Task.Delay(10);
+			}
+			return 20;
+		}
+
+		static async void ProcessDataAsync()
+		{
+			Task<int> promise = SomethingThatTakesTime ();
+			Console.WriteLine("Please wait patiently " +
+				"while I do something important.");
+			int result = await promise;
+			Console.WriteLine("result of long running task was {0}",result);
+		}
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Task task = new Task(ProcessDataAsync);
+			task.Start ();
+			task.Wait ();
+			Console.ReadLine();				
+		}
+	}
+}
---
+++ UIWithAsyncAwait/MainWindow.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Gtk;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnButton1Clicked (object sender, EventArgs e)
+	{
+		Log ("1. Task started");
+		Task task = new Task(ProcessDataAsync);
+		task.Start ();
+		task.Wait ();
+		Log ("3. Task scheduled");
+	}
+
+	async Task<int> SomethingThatTakesTime ()
+	{
+		//await Task.Delay(5000);
+		int counter = 20;
+		while(counter >= 0)
+		{
+			//Console.WriteLine("doing something else {0}",counter--);
+			Log("4. doing something else {0}",counter--);
+			await Task.Delay(500);
+		}
+		return 20;
+	}
+
+	async void ProcessDataAsync()
+	{
+		Task<int> promise = SomethingThatTakesTime ();
+		Log("2. Please wait patiently " + "while I do something important.");
+		int result = await promise;
+		Log("5. result of long running task was {0}",result);
+	}
+
+	private void Log(string format, params object[] values)
+	{
+		string text = string.Format(format, values);
+		string line = string.Format("{0:HH:mm:ss.fff}: {1}\r\n", DateTime.Now, text);
+		logLabel.Text += line;
+	}
+
+	protected void OnButton2Clicked (object sender, EventArgs e)
+	{
+		int numThreads = Process.GetCurrentProcess().Threads.Count;
+		Log ("Responsive ui. thread count {0}",numThreads);
+	}
+}
